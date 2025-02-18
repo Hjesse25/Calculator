89d8b873a456b8630c9f1d036c49ba5c0c0cd76e
Hjesse25 <heinrichjesse28@gmail.com>
2025-02-18 13:57:16 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿bool exit = false;
+
+Console.WriteLine("-------- Console Calculator App --------");
+do
+{
+    try
+    {
+        Console.WriteLine("Enter a number based on desired operation");
+        Console.WriteLine("1. Add");
+        Console.WriteLine("2. Subtract");
+        Console.WriteLine("3. Multiply");
+        Console.WriteLine("4. Divide");
+        Console.WriteLine("5. Exit");
+
+        int choice = int.Parse(Console.ReadLine());
+
+        if (choice == 5)
+        {
+            exit = true;
+            return;
+        }
+
+        Console.Write("Enter first number: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        double result;
+
+        switch (choice)
+        {
+            case 1:
+                result = firstNumber + secondNumber;
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {result}");
+                break;
+            case 2:
+                result = firstNumber - secondNumber;
+                Console.WriteLine($"{firstNumber} - {secondNumber} = {result}");
+                break;
+            case 3:
+                result = firstNumber * secondNumber;
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {result}");
+                break;
+            case 4:
+                if (secondNumber == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero.");
+                }
+                else
+                {
+                    result = firstNumber / secondNumber;
+                    Console.WriteLine($"{firstNumber} + {secondNumber} equals {result}");
+                }
+                break;
+        }
+
+        PressEnter();
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Invalid input, please try again.");
+        PressEnter();
+    }
+
+} while (exit != true);
+
+static void PressEnter()
+{
+    Console.Write("Press enter to continue: ");
+    string enterInput = Console.ReadLine();
+    Console.Clear();
+}
